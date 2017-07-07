f99e3e272a03e2894822fdb420ef5f118631ece4
jinruimin <jinruimin@wepiao.com>
2017-07-07 17:22:56 +0800

    edit

---
+++ UploadFile.php
@@ -0,0 +1,28 @@
+
+<?php
+
+/**
+ * Yii2 uploadpic wrapper
+ *
+ * @category  Web-yii2
+ * @package   yii2-uploadfile
+ * @author    dajinzi <jinruiminzend@sina.com>
+ * @copyright 2017-2020 daijinzi <jinruiminzend@sina.com>
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @version   1.1.3
+ *
+ */
+
+namespace dajinzi\yii2\uploadpic;
+
+use Yii;
+use yii\base\Exception;
+use yii\helpers\Json;
+use yii\web\HttpException;
+
+class UploadFile {
+
+    public function readme() {
+        echo 'this is test file';
+    }
+}
